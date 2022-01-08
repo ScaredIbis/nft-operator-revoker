efe536484e757913e0f344d2df79a475b5a3800c
Chris Martin <chris@crunchycloud.io>
2022-01-08 16:54:26 +1000

    shill rogueibis

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,13 +5,23 @@ import React from "react";
 
 export default function Header({ onShowHelpModal }) {
   return (
-    <div onClick={onShowHelpModal}>
-      <PageHeader
-        title="Revoke NFT Operator Approvals"
-        subTitle="What is this?"
-        style={{ cursor: "pointer" }}
-        onClick
-      />
-    </div>
+    <>
+      <div onClick={onShowHelpModal}>
+        <PageHeader
+          title="Revoke NFT Operator Approvals"
+          subTitle="What is this?"
+          style={{ cursor: "pointer" }}
+          onClick
+        />
+      </div>
+      <p>
+        <strong>
+          Hacked together by{" "}
+          <a target="_blank" href="https://github.com/scaredibis" rel="noreferrer">
+            rogueibis
+          </a>
+        </strong>
+      </p>
+    </>
   );
 }
